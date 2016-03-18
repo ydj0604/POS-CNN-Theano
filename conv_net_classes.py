3a91a5916144b5216816082913ee6f8e3342411e
Dong Jin Yu <yudj0604@gmail.com>
2016-03-18 04:20:06 -0700

    remove unnecessary conv classes

--- conv_net_classes.py
+++ conv_net_classes.py
@@ -31,9 +31,6 @@ def Iden(x):
 
 
 class HiddenLayer(object):
-    """
-    Class for HiddenLayer
-    """
     def __init__(self, rng, input, n_in, n_out, activation, W=None, b=None,
                  use_bias=False):
 
@@ -80,16 +77,6 @@ def _dropout_from_layer(rng, layer, p):
     return output
 
 
-class DropoutHiddenLayer(HiddenLayer):
-    def __init__(self, rng, input, n_in, n_out,
-                 activation, dropout_rate, use_bias, W=None, b=None):
-        super(DropoutHiddenLayer, self).__init__(
-                rng=rng, input=input, n_in=n_in, n_out=n_out, W=W, b=b,
-                activation=activation, use_bias=use_bias)
-
-        self.output = _dropout_from_layer(rng, self.output, p=dropout_rate)
-
-
 class MLPDropout(object):
     """A multilayer perceptron with dropout"""
     def __init__(self, rng, input, layer_sizes, dropout_rates, activations, use_bias=True):
@@ -102,33 +89,9 @@ class MLPDropout(object):
         self.dropout_layers = []
         self.activations = activations
         next_layer_input = input
-        #first_layer = True
+
         # dropout the input
         next_dropout_layer_input = _dropout_from_layer(rng, input, p=dropout_rates[0])
-        layer_counter = 0
-        for n_in, n_out in self.weight_matrix_sizes[:-1]:
-            next_dropout_layer = DropoutHiddenLayer(rng=rng,
-                    input=next_dropout_layer_input,
-                    activation=activations[layer_counter],
-                    n_in=n_in, n_out=n_out, use_bias=use_bias,
-                    dropout_rate=dropout_rates[layer_counter])
-            self.dropout_layers.append(next_dropout_layer)
-            next_dropout_layer_input = next_dropout_layer.output
-
-            # Reuse the parameters from the dropout layer here, in a different
-            # path through the graph.
-            next_layer = HiddenLayer(rng=rng,
-                    input=next_layer_input,
-                    activation=activations[layer_counter],
-                    # scale the weight matrix W with (1-p)
-                    W=next_dropout_layer.W * (1 - dropout_rates[layer_counter]),
-                    b=next_dropout_layer.b,
-                    n_in=n_in, n_out=n_out,
-                    use_bias=use_bias)
-            self.layers.append(next_layer)
-            next_layer_input = next_layer.output
-            #first_layer = False
-            layer_counter += 1
         
         # Set up the output layer
         n_in, n_out = self.weight_matrix_sizes[-1]
@@ -177,96 +140,8 @@ class MLPDropout(object):
         return p_y_given_x
 
 
-class MLP(object):
-    """Multi-Layer Perceptron Class
-
-    A multilayer perceptron is a feedforward artificial neural network model
-    that has one layer or more of hidden units and nonlinear activations.
-    Intermediate layers usually have as activation function tanh or the
-    sigmoid function (defined here by a ``HiddenLayer`` class)  while the
-    top layer is a softamx layer (defined here by a ``LogisticRegression``
-    class).
-    """
-
-    def __init__(self, rng, input, n_in, n_hidden, n_out):
-        """Initialize the parameters for the multilayer perceptron
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.TensorType
-        :param input: symbolic variable that describes the input of the
-        architecture (one minibatch)
-
-        :type n_in: int
-        :param n_in: number of input units, the dimension of the space in
-        which the datapoints lie
-
-        :type n_hidden: int
-        :param n_hidden: number of hidden units
-
-        :type n_out: int
-        :param n_out: number of output units, the dimension of the space in
-        which the labels lie
-
-        """
-
-        # Since we are dealing with a one hidden layer MLP, this will translate
-        # into a HiddenLayer with a tanh activation function connected to the
-        # LogisticRegression layer; the activation function can be replaced by
-        # sigmoid or any other nonlinear function
-        self.hiddenLayer = HiddenLayer(rng=rng, input=input,
-                                       n_in=n_in, n_out=n_hidden,
-                                       activation=T.tanh)
-
-        # The logistic regression layer gets as input the hidden units
-        # of the hidden layer
-        self.logRegressionLayer = LogisticRegression(
-            input=self.hiddenLayer.output,
-            n_in=n_hidden,
-            n_out=n_out)
-
-        # L1 norm ; one regularization option is to enforce L1 norm to
-        # be small
-
-        # negative log likelihood of the MLP is given by the negative
-        # log likelihood of the output of the model, computed in the
-        # logistic regression layer
-        self.negative_log_likelihood = self.logRegressionLayer.negative_log_likelihood
-        # same holds for the function computing the number of errors
-        self.errors = self.logRegressionLayer.errors
-
-        # the parameters of the model are the parameters of the two layer it is
-        # made out of
-        self.params = self.hiddenLayer.params + self.logRegressionLayer.params
-
-
 class LogisticRegression(object):
-    """Multi-class Logistic Regression Class
-
-    The logistic regression is fully described by a weight matrix :math:`W`
-    and bias vector :math:`b`. Classification is done by projecting data
-    points onto a set of hyperplanes, the distance to which is used to
-    determine a class membership probability.
-    """
-
     def __init__(self, input, n_in, n_out, W=None, b=None):
-        """ Initialize the parameters of the logistic regression
-
-    :type input: theano.tensor.TensorType
-    :param input: symbolic variable that describes the input of the
-    architecture (one minibatch)
-    
-    :type n_in: int
-    :param n_in: number of input units, the dimension of the space in
-    which the datapoints lie
-    
-    :type n_out: int
-    :param n_out: number of output units, the dimension of the space in
-    which the labels lie
-    
-    """
-
         # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
         if W is None:
             self.W = theano.shared(
@@ -294,47 +169,13 @@ class LogisticRegression(object):
         self.params = [self.W, self.b]
 
     def negative_log_likelihood(self, y):
-        """Return the mean of the negative log-likelihood of the prediction
-        of this model under a given target distribution.
-
-    .. math::
-    
-    \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
-    \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|} \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
-    \ell (\theta=\{W,b\}, \mathcal{D})
-    
-    :type y: theano.tensor.TensorType
-    :param y: corresponds to a vector that gives for each example the
-    correct label
-    
-    Note: we use the mean instead of the sum so that
-    the learning rate is less dependent on the batch size
-    """
-        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
-        # number of examples (call it n) in the minibatch
-        # T.arange(y.shape[0]) is a symbolic vector which will contain
-        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
-        # Log-Probabilities (call it LP) with one row per example and
-        # one column per class LP[T.arange(y.shape[0]),y] is a vector
-        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
-        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
-        # the mean (across minibatch examples) of the elements in v,
-        # i.e., the mean log-likelihood across the minibatch.
         return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
 
     def errors(self, y):
-        """Return a float representing the number of errors in the minibatch ;
-    zero one loss over the size of the minibatch
-    
-    :type y: theano.tensor.TensorType
-    :param y: corresponds to a vector that gives for each example the
-    correct label
-    """
-
         # check if y has same dimension of y_pred
         if y.ndim != self.y_pred.ndim:
             raise TypeError('y should have the same shape as self.y_pred',
-                ('y', target.type, 'y_pred', self.y_pred.type))
+                ('y', y.type, 'y_pred', self.y_pred.type))
         # check if y is of the correct datatype
         if y.dtype.startswith('int'):
             # the T.neq operator returns a vector of 0s and 1s, where 1
@@ -345,30 +186,7 @@ class LogisticRegression(object):
 
 
 class LeNetConvPoolLayer(object):
-    """Pool Layer of a convolutional network """
-
     def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2), non_linear="tanh"):
-        """
-        Allocate a LeNetConvPoolLayer with shared variable internal parameters.
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dtensor4
-        :param input: symbolic image tensor, of shape image_shape
-
-        :type filter_shape: tuple or list of length 4
-        :param filter_shape: (number of filters, num input feature maps,
-                              filter height,filter width)
-
-        :type image_shape: tuple or list of length 4
-        :param image_shape: (batch size, num input feature maps,
-                             image height, image width)
-
-        :type poolsize: tuple or list of length 2
-        :param poolsize: the downsampling (pooling) factor (#rows,#cols)
-        """
-
         assert image_shape[1] == filter_shape[1]
         self.input = input
         self.filter_shape = filter_shape
@@ -407,9 +225,6 @@ class LeNetConvPoolLayer(object):
         self.params = [self.W, self.b]
         
     def predict(self, new_data, batch_size):
-        """
-        predict for new data
-        """
         img_shape = (batch_size, 1, self.image_shape[2], self.image_shape[3])
         conv_out = conv.conv2d(input=new_data, filters=self.W, filter_shape=self.filter_shape, image_shape=img_shape)
         if self.non_linear=="tanh":
